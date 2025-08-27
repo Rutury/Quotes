d92e3b5c958e2191fe32bf6215a0fec8bc4325ad
Rutury <yury.filyanin@yandex.ru>
2025-08-27 21:06:18 +0700

    make admin pretty

--- QuotesProject/quotes/admin.py
+++ QuotesProject/quotes/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 import quotes.models as models
 # Register your models here.
-admin.site.register(models.Quote)
+
+@admin.register(models.Quote)
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ('quote', 'views', 'likes', 'dislikes', 'weight', 'source')
